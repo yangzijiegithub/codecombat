692720876fd4a67052562688cf52954e4815785d
Nick Winter <livelily@gmail.com>
2017-11-06 11:05:57 -0800

    Quick stab at webpack auto refresh with webpack-dev-server

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -15,7 +15,23 @@ module.exports = (env) => {
     }),
     devtool: 'eval-source-map', // https://webpack.js.org/configuration/devtool/
     devServer: {
-      contentBase: './public'
+      contentBase: './public',
+      inline: true,
+      hot: false,  // Was trying to get live CSS refresh, won't work with ExtractTextPlugin without more fiddling? https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/30
+      port: 3001,
+      proxy: {
+        '**': {
+          target: 'http://localhost:3000',
+          bypass: function(req, res, proxyOptions) {
+            if (/^\/(javascripts|stylesimages|fonts|markdown|templates|lib)/.test(req.path) || /(\.js|\.css|\.html|\.map)$/.test(req.path)) {
+              console.log("serve", req.path, "from webpack-dev-server");
+              return req.path.replace(/^\/dev/, '');
+            }
+            console.log("     serve", req.path, "from normal dev server");
+            return false;
+          }
+        }
+      }
     },
     plugins: baseConfig.plugins.concat([
       new webpack.BannerPlugin({ // Label each module in the output bundle
